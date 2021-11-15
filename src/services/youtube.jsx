c4cbcde4ee5a13f144da04a20fcfe5cd218af626
Brando Vidal <yisusdracon@gmail.com>
2021-11-15 18:16:31 -0500

    feat: Integrate environment variables
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/services/youtube.jsx
+++ src/services/youtube.jsx
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-const baseUrl = 'https://dracon-youtube-downloader-api.herokuapp.com/v1/api/youtube'
-// const baseUrl = 'http://localhost:8000/v1/api/youtube'
+const baseUrl = import.meta.env.VITE_BASE_URL
+const api = `${baseUrl}/youtube`
 
 const search = ({ link }) => {
-  const request = axios.post(`${baseUrl}/search`, { link })
+  const request = axios.post(`${api}/search`, { link })
   return request.then((response) => response.data)
 }
 
 const convert = ({ itag, link }) => {
-  const request = axios.post(`${baseUrl}/convert`, { itag, link })
+  const request = axios.post(`${api}/convert`, { itag, link })
   return request.then((response) => response)
 }
 
