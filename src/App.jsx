bc7f187f6b839ae6b974bcba780f662c43270502
Brando Vidal <yisusdracon@gmail.com>
2021-11-15 01:04:49 -0500

    feat: Add logo and favicon, delete code unless
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./assets/img/logo.png";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Youtube Downloader</h1>
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
