dccee0997585ad5cccdfec7ce1a07ed851833e6a
Brando Vidal <yisusdracon@gmail.com>
2021-11-19 02:37:59 -0500

    feat: Add button to return page and modify styles

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,24 +1,30 @@
+import { Link } from 'wouter'
+
 import style from './style.module.css'
 
 // Components
 import Info from '../Info'
 import ListOfConvert from '../ListOfConvert'
-
 function Result({ data, link }) {
   return (
     data && (
       <section className={style.Result}>
-        <article>
-          volver 
-          </article>
-        <article className={style.streams}>
-          <h3>Formatos de descarga:</h3>
-          <small>Pulse el boton Generar para obtener el formato de descarga seleccionado.</small>
-          <ListOfConvert streams={data.audios} title="Audio" link={link} />
-          <ListOfConvert streams={data.videos} title="Videos" link={link} />
+        <article className={style.content}>
+          <p>¿Desea hacer otra descarga?</p>
+          <Link to="/">
+            <a className={style.link}>Convertir otro video</a>
+          </Link>
         </article>
-        <article>
-          <Info data={data} />
+        <article className={style.content}>
+          <div className={style.column}>
+            <h3>Formatos de descarga:</h3>
+            <small>Pulse el boton Generar para obtener el formato de descarga seleccionado.</small>
+            <ListOfConvert streams={data.audios} title="Audio" link={link} />
+            <ListOfConvert streams={data.videos} title="Videos" link={link} />
+          </div>
+          <div className={style.column}>
+            <Info data={data} />
+          </div>
         </article>
       </section>
     )
